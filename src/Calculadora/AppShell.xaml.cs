999d26a2624297823868213a5435fe7ad4e4f3ce
PhelipeBorba <phelipeborbasilva@gmail.com>
2024-09-05 14:19:12 -0300

    first commit

---
+++ src/Calculadora/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Calculadora
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ src/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Calculadora;
+using xFunc.Maths;
+
+namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        Processor processor = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void AddNumberOrOperatorInCurrentOperation(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+
+            CurrentOperation.Text += button.Text;
+
+            TrySolve();
+        }
+
+        public void BackSpace(object sender, EventArgs args)
+        {
+            try
+            {
+                string tempString = CurrentOperation.Text;
+
+                CurrentOperation.Text = tempString.Remove(tempString.Length - 1);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                CurrentOperation.Text = "";
+            }
+            finally
+            {
+                TrySolve();
+            }
+        }
+
+        public void AddComma(object sender, EventArgs args)
+        {
+            if (!CurrentOperation.Text.Contains(","))
+            {
+                CurrentOperation.Text += ",";
+            }
+
+        }
+
+        public void AllClear(object sender, EventArgs args)
+        {
+            CurrentOperation.Text = "";
+            Result.Text = "";
+        }
+
+        public void SeeResult(object sender, EventArgs args)
+        {
+            CurrentOperation.Text = Result.Text;
+            Result.Text = "";
+        }
+
+        private void TrySolve()
+        {
+            try
+            {
+                Result.Text = processor.Solve(CurrentOperation.Text).ToString();
+            }
+            catch (Exception)
+            {
+                Result.Text = "";
+            }
+        }
+    }
+}
